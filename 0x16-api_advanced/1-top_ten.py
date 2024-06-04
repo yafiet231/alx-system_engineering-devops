11e091555f86495c3df306b37d27fad0b62cce9a
yafiet231 <abieleyasu893@gmail.com>
2024-06-04 18:22:00 +0000

    uploaded

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Alternative-Neat5811)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Alt6242)"
     }
     params = {
         "limit": 10
