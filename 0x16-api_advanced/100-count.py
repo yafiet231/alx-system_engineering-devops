15860ec3af7331d67ea368cc3e5dc47671c9a921
yafiet231 <abieleyasu893@gmail.com>
2024-06-04 21:41:53 +0000

    count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""recursive function that queries the Reddit API, parses the title of all hot articles
-and prints a sorted count of given keywords.
+"""recursive function that queries the Reddit API, parses the title of
+all hot articles and prints a sorted count of given keywords.
 """
 import requests
 
---
+++ 0x16-api_advanced/__init__.py
@@ -0,0 +1,7 @@
+import praw
+
+reddit = praw.Reddit(
+    client_id="2mLbKjUk_5wfgrUK88i3nw",
+    client_secret="KgwqZPPTLED2D3Tcp6AjZyxRI41m-A",
+    user_agent="linux:0x16.api.advanced:v1.0.0 (by /u/Alt6242)"
+)
