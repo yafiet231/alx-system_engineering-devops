37d25844272e1a58d3744392d24ce8d705cbb8fc
yafiet231 <abieleyasu893@gmail.com>
2024-06-04 13:07:01 +0000

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and returns the number of subscribers."""
+"""A function that queries the Reddit API and returns the
+number of subscribers.
+"""
 import requests
 
 
